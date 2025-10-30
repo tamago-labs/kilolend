56cefc29c45bf5e052d642c96559f844647822ec
pisuthd <pisuth.dae@gmail.com>
2025-10-30 12:49:09 +0900

    feat(sc): update new protocol deployment script

--- contracts/src/interest-rates/StablecoinJumpRateModel.sol
+++ contracts/src/interest-rates/StablecoinJumpRateModel.sol
@@ -6,9 +6,9 @@ import "./JumpRateModelV2.sol";
 contract StablecoinJumpRateModel is JumpRateModelV2 {
     
     constructor() JumpRateModelV2(
-        0.01e18,    // baseRatePerYear: 1% 
-        0.04e18,    // multiplierPerYear: 4% slope before kink
-        1.09e18,    // jumpMultiplierPerYear: 109% slope after kink  
+        0.02e18,    // baseRatePerYear: 2% 
+        0.12e18,    // multiplierPerYear: 12% slope before kink
+        1.20e18,    // jumpMultiplierPerYear: 120% slope after kink  
         0.80e18,    // kink: 80% utilization
         msg.sender  // owner
     ) {}
