fa5c066b545c75853d760d159f03d587bb3423ab
pisuthd <pisuth.dae@gmail.com>
2026-02-20 10:53:48 +0900

    fix(bots): import cors module

--- unified_bot/index.js
+++ unified_bot/index.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv'); 
+const cors = require('cors');
 
 // Load environment variables
 dotenv.config();
